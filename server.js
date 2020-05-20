79db965dfd0cb533b7fa66f3a0110f209b1d2879
Ruth Brand <ruthbrandsoprano@gmail.com>
2020-05-20 16:06:49 -0700

    server.js

--- server.js
+++ server.js
@@ -1,14 +1,38 @@
+const { getAllMessages } = require("./getAllMessages");
+
 //needed libaries to run a node server
 const http = require("http");
 const url = require('url');
 const server = http.createServer();
 
-server.listen(3000, () => {
-    console.log('The HTTP server is listening at Port 3000.');
+let messages = [
+    { 'id': 1, 'user': 'brittany storoz', 'message': 'hi there!' },
+    { 'id': 2, 'user': 'bob loblaw', 'message': 'check out my law blog' },
+    { 'id': 3, 'user': 'lorem ipsum', 'message': 'dolor set amet' }
+  ];
+
+function getMessages(input){
+    return messages, response;
+}
+
+  server.listen(3000, () => {
+      console.log('The HTTP server is listening at port 3000.');
   });
-  
+
   server.on('request', (request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('Hello World\n');
-    response.end();
-});
+    if (request.method === 'GET') {
+      return getMessages(response)
+    }
+  
+    else if (request.method === 'POST') {
+      let newMessage = { 'id': new Date() };
+  
+      request.on('data', (data) => {
+        newMessage = Object.assign(newMessage, JSON.parse(data));
+      });
+  
+      request.on('end', () => {
+        addMessage(newMessage, response);
+      });
+    }
+  });
